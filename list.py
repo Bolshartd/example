ccd85df70a78bab80571244afbcb313aff876d24
Bolshartd <bolshartd@gmail.com>
2022-10-08 00:19:57 +0300

    a new version

--- list.py
+++ list.py
@@ -1,6 +1,10 @@
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = []
-for i in a:
-    if i<5:
-        b = i+0
-        print (b)
+def main ():
+    for i in a:
+        if i < 5:
+            b.extend ({i})
+            
+if __name__ == "__main__":
+        main()
+print (b)
