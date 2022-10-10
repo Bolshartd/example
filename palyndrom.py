8dfbb46d49e6d8165679f3fb2e14a7e6241d0307
Bolshartd <bolshartd@gmail.com>
2022-10-10 21:50:09 +0300

    checking words on palyndrom

---
+++ palyndrom.py
@@ -0,0 +1,13 @@
+from audioop import reverse
+
+
+def word():
+    if x == r:
+        return("palyndrom", x)
+    else:
+        return("not palyndrom", x)
+        
+if __name__ == "__main__":
+    x = "wow"
+    r = "".join(reversed(x))
+    print(word())
