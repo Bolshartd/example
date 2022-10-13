d823dc38f14fedb9d593e6203d0154543322b8dc
Bolshartd <bolshartd@gmail.com>
2022-10-13 19:22:32 +0300

    v2 Openning the file from the directory

--- Open_files.py
+++ Open_files.py
@@ -1,9 +1,6 @@
 def file():
-    try:
-        data = open('data.txt', 'r')
+    with open('data.txt') as data:
         data1 = data.read()
         return (data1.replace(",", " "))
-    finally:
-        data.close() 
 if __name__ == "__main__":
     print (file())
