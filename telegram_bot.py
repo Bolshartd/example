9a4badc27ddb8eb1e214e9aad0b9706d0778a543
Bolshartd <bolshartd@gmail.com>
2022-10-25 23:11:02 +0300

    v2 Telegram Bot

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,9 +1,25 @@
 import telebot
+import password
 
-token = '5624394655:AAE1_44oPShmKaGaKw_8uk5nDSpk6aB584U'
-bot = telebot.TeleBot(token)
+token1 = password.token
+bot = telebot.TeleBot(token1)
 
 @bot.message_handler(content_types=['text'])
 def write_message(message):
-   bot.send_message(message.from_user.id, "Приветствую, друг!")
+   if message.text == "1": #число
+      bot.send_message(message.from_user.id, "Я не понимаю чисел, введи текст!")
+   elif message.text == "hey": #текст на английском
+      bot.send_message(message.from_user.id, "Я не понимаю английского языка, пиши по русски")
+   elif message.text == "/":#символы
+      bot.send_message(message.from_user.id, "Что за каракули? Пиши тест")
+   else:
+      bot.send_message(message.from_user.id, "Привет, я Валютный бот\nДля начала обмена введите exchange")
+@bot.message_handler(commands=['exchange'])
+def exchange_message(message):
+   keyboard = telebot.types.InlineKeyboardMarkup()
+   row1 = keyboard.row(telebot.types.InlineKeyboardButton('USD', callback_data='get-USD'))
+   row2 = keyboard.row(telebot.types.InlineKeyboardButton('RUR', callback_data='get-RUR'), telebot.types.InlineKeyboardButton('EUR', callback_data='get-EUR'))
+   keyboard.add (row1, row2)
+   bot.send_message(message.from_use.id, 'Выберите валюту для обмена', reply_markup=keyboard)
+   
 bot.polling(none_stop=True, interval=0)
