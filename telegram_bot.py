4b1e72ed17d91a33695bd198781f17e130f04194
Bolshartd <bolshartd@gmail.com>
2022-10-27 22:10:01 +0300

    v3 Telegram Bot

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,22 +4,24 @@ import password
 token1 = password.token
 bot = telebot.TeleBot(token1)
 
+@bot.message_handler(commands=['start'])
+def exchange_message(message):
+   keyboard = telebot.types.InlineKeyboardMarkup()
+   keyboard.add(telebot.types.InlineKeyboardButton(text = 'USD', callback_data='get-USD'))
+   keyboard.add(telebot.types.InlineKeyboardButton(text = 'RUR', callback_data='get-RUR'), telebot.types.InlineKeyboardButton(text = 'EUR', callback_data='get-EUR'))
+   bot.send_message(message.chat.id, 'Выберите валюту для обмена', reply_markup=keyboard)
+
 @bot.message_handler(content_types=['text'])
 def write_message(message):
    if message.text == "1": #число
-      bot.send_message(message.from_user.id, "Я не понимаю чисел, введи текст!")
+      bot.send_message(message.chat.id, "Я не понимаю чисел, введи текст!")
    elif message.text == "hey": #текст на английском
-      bot.send_message(message.from_user.id, "Я не понимаю английского языка, пиши по русски")
+      bot.send_message(message.chat.id, "Я не понимаю английского языка, пиши по русски")
    elif message.text == "/":#символы
-      bot.send_message(message.from_user.id, "Что за каракули? Пиши тест")
+      bot.send_message(message.chat.id, "Что за каракули? Пиши тест")
+   elif message.text == "ее":#текст на руссом
+      bot.send_message(message.chat.id, "Обычно все люди начинают беседу с привета...")
    else:
-      bot.send_message(message.from_user.id, "Привет, я Валютный бот\nДля начала обмена введите exchange")
-@bot.message_handler(commands=['exchange'])
-def exchange_message(message):
-   keyboard = telebot.types.InlineKeyboardMarkup()
-   row1 = keyboard.row(telebot.types.InlineKeyboardButton('USD', callback_data='get-USD'))
-   row2 = keyboard.row(telebot.types.InlineKeyboardButton('RUR', callback_data='get-RUR'), telebot.types.InlineKeyboardButton('EUR', callback_data='get-EUR'))
-   keyboard.add (row1, row2)
-   bot.send_message(message.from_use.id, 'Выберите валюту для обмена', reply_markup=keyboard)
-   
+      bot.send_message(message.chat.id, "Привет, я Валютный бот\nДля начала обмена нажмите: /start")
+     
 bot.polling(none_stop=True, interval=0)
