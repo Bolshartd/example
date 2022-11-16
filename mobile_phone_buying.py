f32f6fbc66b2b39023a5d431584d365245744e68
Bolshartd <bolshartd@gmail.com>
2022-11-16 09:54:58 +0300

    v2 Choosing the cell phone

--- mobile_phone_buying.py
+++ mobile_phone_buying.py
@@ -13,7 +13,7 @@ class Buying_phone:
         self.tele_type(store_products.phone_type, my_money.value, store_products.color_phone)
     
     def phone_color(self, color_phone):
-        choose_color = input (f"Write the color \n {color_phone.values()}\n")
+        choose_color = input (f"Write the color \n {list(color_phone.values())}\n")
         if choose_color in color_phone.values():
             return color_phone.values()
         else:
@@ -21,13 +21,14 @@ class Buying_phone:
             return Buying_phone.phone_color(self, color_phone)
 
     def tele_type(self, phone_type, value, color_phone):
-        choose_type = input (f"Write the model from the list \n {phone_type.keys()}\n")
+        choose_type = input (f"Write the model from the list \n {list(phone_type.keys())}\n")
         if choose_type in phone_type.keys():
                 if phone_type[choose_type] <= my_money.value:
                     print ("Congrat's with buy")
                     return
                 else:
                     print("Sorry, you don't have enough money")
+                    return
         else:
             print ("Sorry, there is no such type of the phone... Try again\n")
         return Buying_phone.tele_type(self, phone_type, value, color_phone)
